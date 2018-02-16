5bfa061ff92f2fa2f8e5a9e5c2c67f4cb4de546f
slegacheva <32405438+slegacheva@users.noreply.github.com>
2018-02-15 21:45:42 -0800

    Add files via upload

--- src/main/java/DCTestFrameWork_Lib/LogIn.java
+++ src/main/java/DCTestFrameWork_Lib/LogIn.java
@@ -1,5 +1,30 @@
 package main.java.DCTestFrameWork_Lib;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
 public class LogIn {
+	@FindBy(name="identity")
+	public WebElement UserName;
+	
+	@FindBy(name= "password")
+	public WebElement Password;
+	
+	@FindBy(className="btn-authentication")
+	public WebElement LogInButton;
+
+	WebDriver driver;
+	
+	public LogIn(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
 
 }
